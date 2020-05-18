979fd900ddfb98eb400ab709cb1f1e0322209ca8
Tse-Jen (Tony) Lu <yluo0203@gmail.com>
2020-05-17 20:37:55 -0700

    Add files via upload

--- stringPermutations/src/com/company/Main.java
+++ stringPermutations/src/com/company/Main.java
@@ -6,15 +6,43 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String str = "abcdefg";
-        permutation(str);
+        String str = "abc";
+//        permutation2(str);
+        permutation1(str);
     }
 
-    private static void permutation(String str) {
-        helper(str, new HashSet<Character>(), new StringBuilder(""));
+    //Method 1.
+    private static void permutation1 (String str) {
+        helper1(str, 0);
     }
 
-    private static void helper(String str, HashSet<Character> set, StringBuilder result) {
+    private static void helper1(String str, int index) {
+        if (str.length() == index) {
+            System.out.println(str);
+            return;
+        }
+        for (int i = index; i < str.length(); i++) {
+            str = swap(str, i, index);
+            index++;
+            helper1(str, index);
+            index--;
+            str = swap(str, i, index);
+        }
+    }
+
+    private static String swap(String str, int i, int j) {
+        StringBuilder sb = new StringBuilder(str);
+        sb.setCharAt(i, str.charAt(j));
+        sb.setCharAt(j, str.charAt(i));
+        return sb.toString();
+    }
+
+    // Method 2
+    private static void permutation2(String str) {
+        helper2(str, new HashSet<Character>(), new StringBuilder(""));
+    }
+
+    private static void helper2(String str, HashSet<Character> set, StringBuilder result) {
         if (str.length() == result.length()) {
             System.out.println(result);
             return;
@@ -27,7 +55,7 @@ public class Main {
 
             set.add(str.charAt(i));
             result.append(str.charAt(i));
-            helper(str, set, result);
+            helper2(str, set, result);
             set.remove(str.charAt(i));
             result.deleteCharAt(result.length() - 1);
         }

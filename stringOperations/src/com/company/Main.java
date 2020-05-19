edc4bd7e89f4d3331ca9f885dbcbedb63982fd76
Tse-Jen (Tony) Lu <yluo0203@gmail.com>
2020-05-18 21:52:54 -0700

    Add files via upload

--- stringOperations/src/com/company/Main.java
+++ stringOperations/src/com/company/Main.java
@@ -5,22 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         String str = "TonyLu";
-        System.out.println(swap(str, 0, str.length() - 1));
+        System.out.println(swapString(str, 0, str.length() - 1));
     }
 
-    private static void swapString(String str, int i, int j) {
-
-    }
-
-
-    static String swap(String str, int i, int j)
-    {
-        if (j == str.length() - 1)
-            return str.substring(0, i) + str.charAt(j)
-                    + str.substring(i + 1, j) + str.charAt(i);
-
-        return str.substring(0, i) + str.charAt(j)
-                + str.substring(i + 1, j) + str.charAt(i)
-                + str.substring(j + 1, str.length());
+    private static String swapString(String str, int i, int j) {
+        return str.substring(0, i) + str.charAt(j) + str.substring(i + 1, j) + str.charAt(i) + str.substring(j + 1, str.length());
     }
 }

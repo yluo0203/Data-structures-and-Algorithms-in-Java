cc969c9c1428cf925dad40906801642baa562d44
Tse-Jen (Tony) Lu <yluo0203@gmail.com>
2020-05-27 11:08:57 -0700

    Add files via upload

--- charReplacement/src/com/company/Main.java
+++ charReplacement/src/com/company/Main.java
@@ -7,14 +7,28 @@ public class Main {
         StringBuilder str = new StringBuilder("Apple_Banana_Cat_Dog_Elephant_Flower_");
         String s1 = "Elephant_";
         String s2 = "Elk_";
-//        charReplacement(str);
+        charReplacement(str, s1, s2);
+        System.out.println(str);
+
+        s1 = "Flower_";
+        s2 = "Fish_";
+        charReplacement(str, s1, s2);
+        System.out.println(str);
+
+        s1 = "Cat_";
+        s2 = "Cattle_";
+        charReplacement(str, s1, s2);
+        System.out.println(str);
+
+        s1 = "Apple_";
+        s2 = "Alligator_";
         charReplacement(str, s1, s2);
         System.out.println(str);
     }
 
     private static void charReplacement(StringBuilder str, String s1, String s2) {
         if (str == null || str.length() == 0 || s1 == null || s2 == null) return;
-        if (s1.length() < s2.length()) {
+        if (s1.length() <= s2.length()) {
             replaceToLonger(str, s1, s2);
         }else{
             replaceToShorter(str, s1, s2);
@@ -22,7 +36,33 @@ public class Main {
     }
 
     private static void replaceToLonger(StringBuilder str, String s1, String s2) {
+        int fast = str.length() - 1;
+        int diff = s2.length() - s1.length();
+        int count = 0;
+        for (int i = 0; i < str.length() - s1.length() + 1; i++) {
+            if (str.charAt(i) == s2.charAt(0) && isSubString(str, i , s1)) {
+                count++;
+            }
+        }
+        appendChar(str, count * diff);
+        int slow = str.length() - 1;
+        while (fast >= 0) {
+            if (fast >= s1.length() - 1 && isSubString(str, fast - s1.length() + 1, s1)) {
+                int length = s2.length() - 1;
+                while(length >= 0) {
+                    str.setCharAt(slow--, s2.charAt(length--));
+                }
+                fast -= s1.length();
+            } else {
+                str.setCharAt(slow--, str.charAt(fast--));
+            }
+        }
+    }
 
+    private static void appendChar(StringBuilder str, int num) {
+        for (int i = 0; i < num; i++) {
+            str.append(' ');
+        }
     }
 
     private static void replaceToShorter(StringBuilder str, String s1, String s2) {
